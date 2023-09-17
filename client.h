ebb255f4340531cb96f06969f99238988748a3b8
Sakr <49755929+3mosakr@users.noreply.github.com>
2023-09-17 11:04:01 +0300

    Update client.h

--- client.h
+++ client.h
@@ -1,5 +1,10 @@
+
 #ifndef CLIENT_H
 #define CLIENT_H
+
+#define MAX_NUMBER_OF_CLIENTS 3
+
+
 
 typedef struct client
 {
@@ -10,9 +15,18 @@ typedef struct client
 } client_t;
 
 void create_client(void);
+void edit_client_data(void);
+void delete_client(void);
+void print_client_data(void);
+void transfer_money(void);
+void print_all_accounts(void);
+
+/*
+client_t create_client(void);
 void edit_client_data(int client_id);
 void delete_client(int client_id);
 void print_client_data(int client_id);
 void transfer_money(int sender_id, int receiver_id, double amount_of_money);
-
+void print_all_accounts(void);
+*/
 #endif
